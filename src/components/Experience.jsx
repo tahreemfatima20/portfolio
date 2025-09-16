e6ded4b19bfaa882c76808fe70ac85cb6d544c1a
zumarawan <zumar.awan@aigros.com>
2025-09-16 17:09:23 +0500

    fix: resume download issue

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,25 +1,25 @@
 import {
   VerticalTimeline,
   VerticalTimelineElement,
-} from 'react-vertical-timeline-component';
-import { motion } from 'framer-motion';
-import 'react-vertical-timeline-component/style.min.css';
-import { styles } from '../styles';
-import { experiences } from '../constants';
-import { SectionWrapper } from '../hoc';
-import { download, downloadHover, resume } from '../assets';
-import { textVariant } from '../utils/motion';
+} from "react-vertical-timeline-component";
+import { motion } from "framer-motion";
+import "react-vertical-timeline-component/style.min.css";
+import { styles } from "../styles";
+import { experiences } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { download, downloadHover, resume } from "../assets";
+import { textVariant } from "../utils/motion";
 
 const ExperienceCard = ({ experience }) => (
   <VerticalTimelineElement
     contentStyle={{
-      background: '#eaeaec',
-      color: '#292929',
+      background: "#eaeaec",
+      color: "#292929",
       boxShadow:
-        'rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+        "rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
     }}
     contentArrowStyle={{
-      borderRight: '7px solid  #232631',
+      borderRight: "7px solid  #232631",
     }}
     date={
       <div>
@@ -37,14 +37,16 @@ const ExperienceCard = ({ experience }) => (
           className="w-[60%] h-[60%] object-contain"
         />
       </div>
-    }>
+    }
+  >
     <div>
       <h3 className="text-jetLight text-[24px] font-bold font-beckman tracking-[2px]">
         {experience.title}
       </h3>
       <p
         className="text-taupe text-[22px] font-semibold font-overcameBold tracking-[1px]"
-        style={{ margin: 0 }}>
+        style={{ margin: 0 }}
+      >
         {experience.company_name}
       </p>
 
@@ -53,7 +55,8 @@ const ExperienceCard = ({ experience }) => (
           {experience.points.map((point, index) => (
             <li
               key={`experience-point-${index}`}
-              className="text-taupe text-[14px] pl-1 tracking-wider">
+              className="text-taupe text-[14px] pl-1 tracking-wider"
+            >
               {point}
             </li>
           ))}
@@ -82,18 +85,18 @@ const Experience = () => {
           ))}
           <VerticalTimelineElement
             contentStyle={{
-              background: '#eaeaec',
-              color: '#292929',
+              background: "#eaeaec",
+              color: "#292929",
               boxShadow:
-                'rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
+                "rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
             }}
             contentArrowStyle={{
-              borderRight: '7px solid  #232631',
+              borderRight: "7px solid  #232631",
             }}
-            iconStyle={{ background: '#333333' }}
+            iconStyle={{ background: "#333333" }}
             icon={
               <div className="flex justify-center items-center w-full h-full">
                 <img
@@ -102,40 +105,38 @@ const Experience = () => {
                   className="w-[45%] h-[45%] object-contain"
                 />
               </div>
-            }>
-            <button
+            }
+          >
+            <a
+              href="/Tahreem-Fatima.pdf"
+              download="Tahreem-Fatima.pdf"
               className="live-demo flex justify-between 
-              sm:text-[18px] text-[14px] text-timberWolf 
-              font-bold font-beckman items-center py-5 pl-3 pr-3 
-              whitespace-nowrap gap-1 sm:w-[148px] sm:h-[58px] 
-              w-[125px] h-[46px] rounded-[10px] bg-jetLight 
-              sm:mt-[22px] mt-[16px] hover:bg-battleGray 
-              hover:text-eerieBlack transition duration-[0.2s] 
-              ease-in-out"
-              onClick={() =>
-                window.open(
-                  '/src/assets/Tahreem Fatima.pdf', //paste the link to your resume here
-                  '_blank'
-                )
-              }
+    sm:text-[18px] text-[14px] text-timberWolf 
+    font-bold font-beckman items-center py-5 pl-3 pr-3 
+    whitespace-nowrap gap-1 sm:w-[148px] sm:h-[58px] 
+    w-[125px] h-[46px] rounded-[10px] bg-jetLight 
+    sm:mt-[22px] mt-[16px] hover:bg-battleGray 
+    hover:text-eerieBlack transition duration-[0.2s] 
+    ease-in-out"
               onMouseOver={() => {
                 document
-                  .querySelector('.download-btn')
-                  .setAttribute('src', downloadHover);
+                  .querySelector(".download-btn")
+                  ?.setAttribute("src", downloadHover);
               }}
               onMouseOut={() => {
                 document
-                  .querySelector('.download-btn')
-                  .setAttribute('src', download);
-              }}>
+                  .querySelector(".download-btn")
+                  ?.setAttribute("src", download);
+              }}
+            >
               MY RESUME
               <img
                 src={download}
                 alt="download"
                 className="download-btn sm:w-[26px] sm:h-[26px] 
-                w-[23px] h-[23px] object-contain"
+    w-[23px] h-[23px] object-contain"
               />
-            </button>
+            </a>
           </VerticalTimelineElement>
         </VerticalTimeline>
       </div>
@@ -143,4 +144,4 @@ const Experience = () => {
   );
 };
 
-export default SectionWrapper(Experience, 'work');
+export default SectionWrapper(Experience, "work");
